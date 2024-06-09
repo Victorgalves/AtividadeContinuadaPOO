31b2b1e222def81aae0c9d2e9fa26a3032be159b
Talita Fraga <talitafraga@MacBook-Air-de-Talita.local>
2024-06-09 00:26:36 -0300

    fix:implementaçao DAO generico em VooDAO

--- src/br/edu/cesarschool/cc/poo/ac/passagem/VooDAO.java
+++ src/br/edu/cesarschool/cc/poo/ac/passagem/VooDAO.java
@@ -1,54 +1,37 @@
 package br.edu.cesarschool.cc.poo.ac.passagem;
 
-import java.io.Serializable;
+import br.edu.cesarschool.cc.poo.ac.utils.SuperDAO;
+import br.edu.cesarschool.cc.poo.ac.utils.Registro;
 
-import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
+public class VooDAO extends SuperDAO<Voo> {
 
-public class VooDAO  {
-	private CadastroObjetos cadastro = new CadastroObjetos(Voo.class);
+	@Override
+	public Class<Voo> obterTipo() {
+		return Voo.class;
+	}
+
 	public Voo buscar(String idVoo) {
-		return (Voo)cadastro.buscar(idVoo);
-	}
-	private String obterIdUnicoVoo(Voo voo) {
-		return voo.getCompanhiaAerea() + voo.getNumeroVoo();
-	}
-	public boolean incluir(Voo voo) {
-		Voo cli = buscar(obterIdUnicoVoo(voo));
-		if (cli == null) {
-			cadastro.incluir(voo, obterIdUnicoVoo(voo));
-			return true;
-		} 
-		return false; 
-	}
-	public boolean alterar(Voo voo) {
-		Voo cli = buscar(obterIdUnicoVoo(voo));
-		if (cli != null) {
-			cadastro.alterar(voo, obterIdUnicoVoo(voo));
-			return true;
-		} 
-		return false; 
-	}
-	public boolean excluir(String idVoo) {
-		Voo cli = buscar(idVoo);
-		if (cli != null) {
-			cadastro.excluir(idVoo);
-			return true;
-		} 
-		return false; 
-	}
-	public Voo[] buscarTodos() {
-		Serializable[] res = cadastro.buscarTodos();
-		if (res == null) {
-			return null;
-		} else {
-			Voo[] voos = new Voo[res.length];
-			int i = 0;
-			for (Serializable reg : res) {
-				voos[i] = (Voo)reg;
-				i++;
-			}
-			return voos;
-		}
+		return daoGenerico.buscar(idVoo);
 	}
 
+	public boolean incluir(Voo voo) {
+		return daoGenerico.incluir(voo);
+	}
+
+	public boolean alterar(Voo voo) {
+		return daoGenerico.alterar(voo);
+	}
+
+	public boolean excluir(String idVoo) {
+		return daoGenerico.excluir(idVoo);
+	}
+
+	public Voo[] buscarTodos() {
+		Registro[] registros = daoGenerico.buscarTodos();
+		Voo[] voos = new Voo[registros.length];
+		for (int i = 0; i < registros.length; i++) {
+			voos[i] = (Voo) registros[i];
+		}
+		return voos;
+	}
 }
